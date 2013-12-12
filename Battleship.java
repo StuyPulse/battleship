46d4683b14882cb94b4fed5e8da7d8ce8f903207
ChesleyTan <ChesleyTan97@gmail.com>
2013-12-12 18:48:33 -0500

    Move Scanner into main()

--- Battleship.java
+++ Battleship.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Battleship{
-	Scanner scan1 = new Scanner(System.in);
-	String input = scan1.next();
 	public static void main(String[] args){
-
+		Scanner scan1 = new Scanner(System.in);
+		String input = scan1.next();
 	}	
 }
