c8fb25020298090045cf095d241ecb83a0bf1af3
Guest <guest-wCHl4z@cslab4-23.stuy.edu>
2014-01-09 17:05:53 -0500

    fixed input

--- Driver.java
+++ Driver.java
@@ -3,47 +3,28 @@ import java.util.Scanner;
 public class Driver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String inputLetterCoor = sc.next();
-		int inputNumberCoor;
-		if (sc.hasNextInt()) {
-			inputNumberCoor = sc.nextInt();
-		}
-		else {
-			System.out.println("Stop being evil and put in a number!");
-			inputNumberCoor = 1;
-		}
+		char guessLetter='X';
+		int guessNumber=-1;
+		while (guessLetter=='X') {
+		    System.out.println("Enter a letter coordinate A-E: ");
+		    guessLetter = sc.next().charAt(0);
+		    if((int)guessLetter<'A' || (int)guessLetter>'E')
+		    {
+			guessLetter='X';
+		    }
+	        }
+		while (guessNumber==-1) {
+		    System.out.println("Enter a number coordinate 1-5: ");
+		    if (sc.hasNextInt()) {
+			guessNumber = sc.nextInt();
+		    }
 
-		String guessLetter;
-		int guessNumber;
+		    if(guessNumber<1 || guessNumber>5)
+		    {
+			guessNumber=-1;
+		    }
+		}	
 
-		if (inputLetterCoor.equalsIgnoreCase("A")) {
-			guessLetter = "A";		
-		}
-		else if (inputLetterCoor.equalsIgnoreCase("B")) {
-			guessLetter = "B";		
-		}
-		else if (inputLetterCoor.equalsIgnoreCase("C")) {
-			guessLetter = "C";		
-		}
-		else if (inputLetterCoor.equalsIgnoreCase("D")) {
-			guessLetter = "D";		
-		}
-		else if (inputLetterCoor.equalsIgnoreCase("E")) {
-			guessLetter = "E";		
-		}
-		else {
-			System.out.println("Please input a coordinate from A to E");
-		}
-
-		if (inputNumberCoor > 0 && inputNumberCoor < 6) {
-			guessNumber = inputNumberCoor;
-		}
-		else {
-			System.out.println("Please input a coordinate from 1 to 5");
-		}
-
-		
 	
-		
 	}
 }	
